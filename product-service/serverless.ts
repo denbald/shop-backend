445ec4fae4e4028e3f41a0d5370e9bb3fadb4d0c
Denis Baldych <denisbaldych@gmail.com>
2023-03-19 21:59:57 +0400

    feat: add information about tables to serverless and add data population script

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -33,10 +33,59 @@ const serverlessConfiguration: AWS = {
       platform: 'node',
       concurrency: 10,
     },
+    productsTable: 'products',
+    stocksTable: 'stocks',
     autoswagger: {
       apiType: 'http',
     },
   },
+  resources: {
+    Resources: {
+      ProductsTable: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: '${self:custom.productsTable}',
+          AttributeDefinitions: [
+            { AttributeName: 'id', AttributeType: 'S' },
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'id',
+              KeyType: 'HASH',
+            },
+          ],
+          BillingMode: 'PROVISIONED',
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1,
+          },
+        },
+      },
+      StocksTable: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: '${self:custom.stocksTable}',
+          AttributeDefinitions: [
+            {
+              AttributeName: 'product_id',
+              AttributeType: 'S',
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'product_id',
+              KeyType: 'HASH',
+            },
+          ],
+          BillingMode: 'PROVISIONED',
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1,
+          },
+        },
+      },
+    },
+  }
 };
 
 module.exports = serverlessConfiguration;
